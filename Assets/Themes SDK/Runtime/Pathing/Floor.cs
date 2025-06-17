60cc30f51db79668c934849e9ceca57db4f7799e
Nicholas-Wilkinson <nicholas.wilkinson@live.nl>
2025-06-17 12:14:19 +0200

    Added pathing to the Themes SDK
    These scripts allow the theme map to define terrain features without having to faff about with layers themselves.

---
+++ Assets/Themes SDK/Runtime/Pathing/Floor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace OpenUp.Environment.Pathing
+{
+    public class Floor : MonoBehaviour, ITempLayer
+    {
+        private int floorLayer;
+        private int existingLayer;
+        
+        private IPathing pathing;
+
+        private void OnDestroy()
+        {
+            pathing.RemoveFloor(this);
+        }
+
+        public void SetPathing(IPathing pathing)
+        {
+            this.pathing = pathing;
+            pathing.AddFloor(this);
+        }
+
+        public void SetLayer(int layer)
+        {
+            if (!enabled) return;
+
+            existingLayer = gameObject.layer;
+            gameObject.layer = layer;
+        }
+
+        public void ResetLayer() => gameObject.layer = existingLayer;
+    }
+}
---
+++ Assets/Themes SDK/Runtime/Pathing/Wall.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace OpenUp.Environment.Pathing
+{
+    public class Wall : MonoBehaviour, ITempLayer
+    {
+        [Serializable]
+        public class OnWallHit : UnityEvent<RaycastHit> { }
+
+        [SerializeField] public OnWallHit onWallHit = new OnWallHit();
+
+        private int existingLayer;
+        private IPathing pathing;
+
+        private void OnDestroy()
+        {
+            if (pathing != null)
+                pathing.RemoveWall(this);
+        }
+
+        public void SetPathing(IPathing pathing)
+        {
+            this.pathing = pathing;
+            pathing.AddWall(this);
+        }
+
+        public void SetLayer(int layer)
+        {
+            if (!enabled) return;
+            
+            existingLayer = gameObject.layer;
+            gameObject.layer = layer;
+        }
+
+        public void ResetLayer() => gameObject.layer = existingLayer;
+    }
+}
